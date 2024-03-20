f3192f448bc287159bca68843e144214ba63b80d
dreamth <d.dimcard@gmail.com>
2024-03-20 21:14:23 +0200

    somerandomshit

--- src/commands/manage/profile.js
+++ src/commands/manage/profile.js
@@ -15,13 +15,13 @@ module.exports = {
 		.setDescription('Get information about your pfofile.')
 		.addUserOption(option =>
 			option.setName('username')
-				  .setDescription('Username of the required profile') ),
+				.setDescription('Username of the required profile')),
 	async execute(interaction, client) {
 		const currentUser = new Object(userProfile);
 		let userLink;
-		if(!interaction.options.getUser('username')){
+		if (!interaction.options.getUser('username')) {
 			userLink = interaction.user;
-		} else{
+		} else {
 			userLink = interaction.options.getUser('username');
 		}
 		currentUser.username = userLink.tag;
@@ -31,21 +31,21 @@ module.exports = {
 		await interaction.reply(".");
 		await interaction.deleteReply();
 		const message = new EmbedBuilder()
-						.setTitle(`${currentUser.username}'s profile`)
-						.setAuthor({name: "a"})
-						.setDescription(`@${currentUser.username} @${currentUser.displayName} @${currentUser.globalName}`)
-						.setColor(await this.randomColor())
-						.setImage(currentUser.userPfp)
-						.setThumbnail("https://media1.tenor.com/m/yDv6dGXKzy0AAAAC/homura-akemi-akemi-homura.gif")
-						.addFields(
-							{ name: 'Regular field title', value: 'Some value here' },
-							{ name: '\u200B', value: '\u200B' },
-							{ name: 'Inline field title', value: 'Some value here', inline: true },
-							{ name: 'Inline field title', value: 'Some value here', inline: true },
-						)
-						.setTimestamp()
-						.setFooter( { text: `@${interaction.user.tag}` });
+			.setTitle(`${currentUser.username}'s profile`)
+			.setAuthor({ name: "a" })
+			.setDescription(`@${currentUser.username} @${currentUser.displayName} @${currentUser.globalName}`)
+			.setColor(await this.randomColor())
+			.setImage(currentUser.userPfp)
+			.setThumbnail("https://media1.tenor.com/m/yDv6dGXKzy0AAAAC/homura-akemi-akemi-homura.gif")
+			.addFields(
+				{ name: 'Regular field title', value: 'Some value here' },
+				{ name: '\u200B', value: '\u200B' },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+			)
+			.setTimestamp()
+			.setFooter({ text: `@${interaction.user.tag}` });
 		await interaction.channel.send({ embeds: [message] });
 	},
-	async randomColor () { return Math.floor(Math.random()*16777215).toString(16) },
+	async randomColor() { return Math.floor(Math.random() * 16777215).toString(16) },
 };
